dac6ed2c2891bc8057624bfdad04c736c375e0a6
BinaryBitTechnologiesKhumbuSystem <motloung.thami@gmail.com>
2023-07-19 08:56:37 +0200

    Add project files.

---
+++ Bank Application OOP/BankAccount.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Application_OOP
+{
+    internal class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = balance + item.Amount;
+                }
+                return balance;
+
+            }
+
+        }
+
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+
+        //My Constructor
+        public BankAccount(string owner, decimal initialBalance)
+        {
+            this.Owner = owner;
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+        }
+
+        //Makes deposits into the account
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+
+        }
+
+
+        //Withdrawal 
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+
+        }
+
+        //Gets Account history
+        public string GetAccountHistory()
+        {
+
+            var report = new StringBuilder();
+
+            //HEADER
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+
+                report.AppendLine($"{item.Date.ToShortDateString()}\tR{item.Amount}\t{item.Notes}");
+
+            }
+
+            return report.ToString();
+
+        }
+    }
+}
---
+++ Bank Application OOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Application_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Spha", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with R{account.Balance}.");
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(100, DateTime.Now, "Todays's Lunch");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+            //Test that the initial Amount must be positive
+            /*try
+            {
+                var invalidAccount = new BankAccount("Invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating acccount with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            
+
+            //Test For a Negative Balance
+            try
+            {
+
+               account.MakeWithdrawal(80000, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdrwa");
+                Console.WriteLine(e.ToString());
+            }*/
+
+
+
+
+
+
+        }
+    }
+}
